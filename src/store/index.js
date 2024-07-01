91a2f66d87c50ca7f8c104def0ff002deb03f724
rafidaky <rafidakn@gmail.com>
2024-07-01 23:01:52 +0200

    redux toolkit query imlp

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
+
+// makes store accessible through console for dev purposes
+// window.store = store
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery, useAddAlbumMutation } from "./apis/albumsApi";
